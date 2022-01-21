bfee3b70a27b186efc5777b6b38e76a77ae1c4de
eleung32 <52718704+eleung32@users.noreply.github.com>
2022-01-21 11:49:40 -0500

    changed classname and added css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 const formReducer = (state, event) => {
  return {
     ...state,
-    [event.name]: event.value
+    [event.name]: event.value,    
   }
 }
 
 function App() {
-  const [formData, setFormData] = useReducer(formReducer, {
-  });
+  const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
 
   const handleSubmit = event => {
@@ -23,15 +22,14 @@ function App() {
   }
 
   const handleChange = event => {
-    const isCheckbox = event.target.type === 'checkbox';
     setFormData({
       name: event.target.name,
-      value: isCheckbox ? event.target.checked : event.target.value,
+      value: event.target.value
     });
   }
 
   return(
-    <div className="wrapper">
+    <div className="container">
       <h1>Get an Instant Quotation</h1>
       {submitting &&
         <div>
@@ -49,7 +47,7 @@ function App() {
           <label>
             <p>Type of service</p>
             <select name="service" onChange={handleChange} required>
-                <option value="">--Please choose an option--</option>
+                <option value="" >--Please choose an option--</option>
                 <option value="financial statements & tax returns preparation">Financial statements & tax returns preparation</option>
                 <option value="incorporation & business accounts registration">Incorporation & business accounts registration</option>
                 <option value="personal tax preparation">Personal tax preparation</option>
@@ -58,8 +56,9 @@ function App() {
           
           <label hidden={formData.service !== 'financial statements & tax returns preparation'}>
             <p>Category</p>
-            <select name="category" onChange={handleChange} >
-                <option value="">--Please choose an option--</option>
+            <select name="category" onChange={handleChange}
+             required={formData.service === 'financial statements & tax returns preparation'} >
+                <option value="" >--Please choose an option--</option>
                 <option value="compilation">Compilation</option>
                 <option value="review-engagement">Review Engagement</option>
                 <option value="audit-engagement">Audit Engagement</option>
@@ -68,7 +67,7 @@ function App() {
           
           <label hidden={formData.category !== 'compilation'}>
             <p>Transactions per year</p>
-            <select name="compilationTxn" onChange={handleChange} >
+            <select name="compilationTxn" onChange={handleChange} required={formData.category === 'compilation'} >
               <option value="">--Please choose an option--</option>
               <option value="fifty-txn">0-50</option>
               <option value="onefifty-txn">50-150</option>
@@ -80,7 +79,7 @@ function App() {
         <fieldset disabled={submitting}>
           <label>
             <p>Email address</p>
-            <input type="email" id="email" name="email" placeholder="email@address.com" required
+            <input type="email" id="email" name="email" placeholder="email@address.com" 
              onChange={handleChange} />
           </label>
         </fieldset>
