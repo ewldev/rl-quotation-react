f3ba4140b1fcaa9feff85476a127dfda3477ae82
eleung32 <52718704+eleung32@users.noreply.github.com>
2022-01-27 12:46:11 -0500

    renamed div2 div3 to service2 service3

--- client/src/App.js
+++ client/src/App.js
@@ -18,18 +18,18 @@ function App() {
   const handleSubmit = event => {
     const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService'); 
     event.preventDefault();
-    setSubmitting(true); 
-    calculate();   
+    calculate(); 
+    setSubmitting(true);      
     console.log('handleSubmit oneFiftyPlusTxn.value',oneFiftyPlusTxn.value)
   }
 
   const handleChange1 = event =>  {
-    const div2 = document.getElementById('div2');
-    const div3 = document.getElementById('div3');
+    const service2 = document.getElementById('service2');
+    const service3 = document.getElementById('service3');
     const result = document.getElementById('result');
     resetResultValue();
-    div2.style.display = 'none';
-    div3.style.display = 'none';
+    service2.style.display = 'none';
+    service3.style.display = 'none';
     result.style.display = 'inline';  
     setFormData({
       name: event.target.name,
@@ -66,8 +66,9 @@ function App() {
 
 function resetResultValue() { 
   const result = document.getElementById('result');
-  console.log('resetResultValue1',result.value)
-  result.value = '';    
+  console.log('resetResultValue1',result.value);
+  result.value = '';  
+ 
   console.log('resetResultValue2',result.value)
 }
 
@@ -82,6 +83,120 @@ const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService');
   formData.oneFiftyService = ''; 
   formData.oneFiftyPlusService='';   
 }
+
+// process quotation value 
+function calculate() { 
+  // const service = document.getElementById('service');
+  const category = document.getElementById('category')
+  const compilation = document.getElementById('compilationTxn');
+  const fiftyTxn = document.getElementById('fiftyService');
+  const oneFiftyTxn = document.getElementById('oneFiftyService');  
+  const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService'); 
+  const result = document.getElementById('result');
+  
+    switch(formData.service) {
+      case 'incorporation & business accounts registration':
+        document.getElementById('service2').style.display = 'block';               
+        formData.result = '$800 - business trade name add $100';
+        result.style.display = 'none'; 
+        break;
+      case 'personal tax preparation':
+        document.getElementById('service3').style.display = 'block'; 
+        formData.result = `<p>standard $150 <br>
+        capital gain/loss add $100 <br>
+        self-employed business add $250 <br>
+        foreign property declaration add $150            
+      </p>`
+        result.style.display = 'none'; 
+        break;
+      default:
+        break;  
+    }
+    switch(compilation.value) {
+      case 'fifty-txn':
+        result.value = '$1500-$1800';
+        break;
+      case 'onefifty-txn':
+        result.value = '$1800-$2500';
+        break;
+      case 'onefiftyplus-txn':
+        result.value  = '$2500-$3500'; 
+        break;
+      default:
+         break;  
+    }
+    switch(category.value) {
+      case 'review-engagement':
+        result.value = '$5500-$7500';
+        break;
+      case 'audit-engagement':
+        result.value = '$9000-$12000';  
+        break;
+      default:
+        break;  
+    }
+    switch(fiftyTxn.value) {      
+      case 'consulting-business1':
+        result.value  = '$1500';  
+        break;
+      case 'holdings-company1':
+        result.value = '$1700'; 
+        break;
+      default:
+        break;   
+    }    
+    switch(oneFiftyTxn.value) {
+      case 'consulting-business2':
+        result.value = '$1800';
+        break;
+      case 'holdings-company2':
+        result.value = '$2000';
+        break;
+      case 'trading-retail2':
+        result.value = '$2500';
+        break;
+      case 'mortgage-insurance-broker2':
+        result.value = '$1800';
+        break;
+      case 'home-renovation-contractor2':
+        result.value = '$2300';
+        break;
+      case 'medical-professional-corporation2':
+        result.value = '$2500';
+        break;
+      default:
+        break;  
+    }  
+    switch(oneFiftyPlusTxn.value) {
+      case 'consulting-business3':
+        result.value = '$2500';
+        break;
+      case 'holdings-company3':
+        result.value = '$2700';
+        break;
+      case 'trading-retail3':
+        result.value = '$3200'; 
+        break;
+      case 'manufacturing3':
+        result.value = '$3500';
+        break;
+      case 'restaurant3':
+        result.value = '$3500';
+        break;  
+      case 'mortgage-insurance-broker3':        
+        result.value = '$2500';
+        break;
+      case 'home-renovation-contractor3':
+        result.value = '$3000';
+        break;
+      case 'medical-professional-corporation3':
+        result.value = '$3500';
+        break;
+      default:
+        break;    
+    }    
+  }  
+
 
   return(
     <div id="container">
@@ -204,13 +319,13 @@ const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService');
           <output name="result" type="text" id="result"></output> 
           
           {/* Incorporation & business accounts registration */}
-          <div id="div2">
+          <div id="service2">
           <p>$800 <br></br>
           business trade name add $100</p>
           </div>
 
           {/*Personal tax preparation*/}
-          <div id="div3">
+          <div id="service3">
             <p>standard $150 <br></br>
                capital gain/loss add $100  <br></br>
                self-employed business add $250 <br></br>
@@ -225,118 +340,6 @@ const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService');
   )
 }
 
-// process quotation value 
-function calculate() { 
-  const service = document.getElementById('service');
-  const category = document.getElementById('category')
-  const compilation = document.getElementById('compilationTxn');
-  const fiftyTxn = document.getElementById('fiftyService');
-  const oneFiftyTxn = document.getElementById('oneFiftyService');  
-  const oneFiftyPlusTxn = document.getElementById('oneFiftyPlusService'); 
-  const result = document.getElementById('result');
-  
-    switch(service.value) {
-      case 'incorporation & business accounts registration':
-        document.getElementById('div2').style.display = 'block';               
-        result.value = '$800 - business trade name add $100';
-        result.style.display = 'none'; 
-        break;
-      case 'personal tax preparation':
-        document.getElementById('div3').style.display = 'block'; 
-        result.value = `<p>standard $150 <br>
-        capital gain/loss add $100 <br>
-        self-employed business add $250 <br>
-        foreign property declaration add $150            
-      </p>`
-        result.style.display = 'none'; 
-        break;
-      default:
-        break;  
-    }
-    switch(compilation.value) {
-      case 'fifty-txn':
-        result.value = '$1500-$1800';
-        break;
-      case 'onefifty-txn':
-        result.value = '$1800-$2500';
-        break;
-      case 'onefiftyplus-txn':
-        result.value  = '$2500-$3500'; 
-        break;
-      default:
-         break;  
-    }
-    switch(category.value) {
-      case 'review-engagement':
-        result.value = '$5500-$7500';
-        break;
-      case 'audit-engagement':
-        result.value = '$9000-$12000';  
-        break;
-      default:
-        break;  
-    }
-    switch(fiftyTxn.value) {      
-      case 'consulting-business1':
-        result.value  = '$1500';  
-        break;
-      case 'holdings-company1':
-        result.value = '$1700'; 
-        break;
-      default:
-        break;   
-    }    
-    switch(oneFiftyTxn.value) {
-      case 'consulting-business2':
-        result.value = '$1800';
-        break;
-      case 'holdings-company2':
-        result.value = '$2000';
-        break;
-      case 'trading-retail2':
-        result.value = '$2500';
-        break;
-      case 'mortgage-insurance-broker2':
-        result.value = '$1800';
-        break;
-      case 'home-renovation-contractor2':
-        result.value = '$2300';
-        break;
-      case 'medical-professional-corporation2':
-        result.value = '$2500';
-        break;
-      default:
-        break;  
-    }  
-    switch(oneFiftyPlusTxn.value) {
-      case 'consulting-business3':
-        result.value = '$2500';
-        break;
-      case 'holdings-company3':
-        result.value = '$2700';
-        break;
-      case 'trading-retail3':
-        result.value = '$3200'; 
-        break;
-      case 'manufacturing3':
-        result.value = '$3500';
-        break;
-      case 'restaurant3':
-        result.value = '$3500';
-        break;  
-      case 'mortgage-insurance-broker3':        
-        result.value = '$2500';
-        break;
-      case 'home-renovation-contractor3':
-        result.value = '$3000';
-        break;
-      case 'medical-professional-corporation3':
-        result.value = '$3500';
-        break;
-      default:
-        break;    
-    }    
-  }  
 
 // close buttons
 window.onload = function(){
